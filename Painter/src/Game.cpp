409e37933da1f1a1374be37bb9eca81a065c1655
gcaino <german.caino@hotmail.com>
2018-02-04 07:12:02 -0300

    [MODIFIED] Clase Game

--- Painter/src/Game.cpp
+++ Painter/src/Game.cpp
@@ -1,17 +1,42 @@
+#include <iostream>
 #include "Game.h"
 
 Game::Game()
 	: _window()
-	, _lifes(MAX_LIFES)
+	, _lives(MAX_LIVES)
 	, _score(0)
+	, _paused(false)
 {
 	_window = new sf::RenderWindow(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "MyA I - TRABAJO FINAL");
 	_window->setFramerateLimit(FPS);
+
+	loadTexturesAndSetSprites();
 }
 
 Game::~Game()
 {
 	delete _window;
+}
+
+void Game::loadTexturesAndSetSprites()
+{
+	_backgroundTexture.loadFromFile("assets/textures/spr_background.jpg");
+	_backgroundSprite.setTexture(_backgroundTexture);
+
+	_scoreBarTexture.loadFromFile("assets/textures/spr_scorebar.jpg");
+	_scoreBarSprite.setTexture(_scoreBarTexture);
+	_scoreBarSprite.setPosition(10.f, 10.f);
+
+	_liveTexture.loadFromFile("assets/textures/spr_lives.png");
+	for (int i = 0; i < MAX_LIVES; i++)
+	{
+		_liveSprite[i].setTexture(_liveTexture);
+		_liveSprite[i].setPosition(10.f + (_liveSprite[i].getGlobalBounds().width * i), 
+			_scoreBarSprite.getPosition().y + _scoreBarSprite.getGlobalBounds().height + 10.f);
+	}
+	
+	_gameOverTexture.loadFromFile("assets/textures/spr_gameover_click.png");
+	_gameOverSprite.setTexture(_gameOverTexture);
 }
 
 void Game::loop()
@@ -24,9 +49,10 @@ void Game::loop()
 		processEvents();
 
 		deltaTime = clock.restart();
-		update(deltaTime);
-
-		render();
+		if (!_paused)
+			update(deltaTime);
+		
+		draw();
 	}
 }
 
@@ -35,25 +61,48 @@ void Game::processEvents()
 	sf::Event event;
 	while (_window->pollEvent(event))
 	{
-		switch (event.type)
+		if (event.type == sf::Event::Closed ||
+			(event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
 		{
-		case sf::Event::Closed:
 			_window->close();
-			break;
-		default:
-			break;
+		}
+		else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::P)
+		{
+			pause();
 		}
 	}
 }
 
-void Game::update(sf::Time deltaTime)
+void Game::pause()
 {
-
+	_paused = !_paused;
 }
 
-void Game::render()
+void Game::update(sf::Time deltaTime)
+{
+	_timer += deltaTime;
+	if (_timer >= sf::seconds(5.f))
+	{
+		--_lives;
+		_timer = sf::Time::Zero;
+	}
+}
+
+void Game::draw()
 {
 	_window->clear();
 
+	_window->draw(_backgroundSprite);
+	_window->draw(_scoreBarSprite);
+	drawLives();
+
 	_window->display();
 }
+
+void Game::drawLives() const
+{
+	for (int i = 0; i < _lives; i++)
+		_window->draw(_liveSprite[i]);
+}
+
+
--- Painter/src/Game.h
+++ Painter/src/Game.h
@@ -13,16 +13,30 @@ public:
 private:
 	void processEvents();
 	void update(sf::Time deltaTime);
-	void render();
+	void draw();
+
+	void loadTexturesAndSetSprites();
+	void drawLives() const;
+	void pause();
 
 	const int WINDOW_WIDTH = 800;
-	const int WINDOW_HEIGHT = 600;
+	const int WINDOW_HEIGHT = 480;
 	const int FPS = 60;
-	const int MAX_LIFES = 5;
+	static const int MAX_LIVES = 5;
 
 	sf::RenderWindow*	_window;
-	int					_lifes;
+	int					_lives;
 	int					_score;
+	bool				_paused;
+
+	sf::Texture			_backgroundTexture;
+	sf::Sprite			_backgroundSprite;
+	sf::Texture			_liveTexture;
+	sf::Sprite			_liveSprite[MAX_LIVES];
+	sf::Texture			_scoreBarTexture;
+	sf::Sprite			_scoreBarSprite;
+	sf::Texture			_gameOverTexture;
+	sf::Sprite			_gameOverSprite;
 };
 
 #endif // !GAME_H
